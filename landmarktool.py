52e146d1798d962330e46cd608f26c252f1080dd
Melodi <83541357+Melodi3D@users.noreply.github.com>
2026-08-19 11:07:37 -0400

    Update landmarktool.py

--- landmarktool.py
+++ landmarktool.py
@@ -1,9 +1,17 @@
 """
 Landmark Tool by Melodi
 """
-# Explanation:
-# imports maya commands
-from maya import cmds
+
+import maya.cmds as cmds
+import maya.mel as mel
+from maya import OpenMayaUI as omui
+from shiboken6 import wrapInstance
+from PySide6 import QtUiTools, QtCore, QtGui, QtWidgets
+from functools import partial # optional, for passing args during signal function calls
+import sys
+import os
+
+# landmark functions
 
 # color presets
 red = (1.0, 0.0, 0.0)
@@ -15,6 +23,7 @@ magenta = (1.0, 0.0, 1.0)
 cyan = (0.0, 1.0, 1.0)
 pink = (1.0, 0.4, 0.7)
 
+#function for confirming faces are selected
 def faces_confirm():
     cmds.confirmDialog(
         title="Landmark Tool",
@@ -65,4 +74,53 @@ def create_landmark(colors):
 
     type="double3")
 
-create_landmark(red)
+
+class landmarkTool(QtWidgets.QWidget):
+    """
+    Creates tool window
+    """
+    window = None
+
+    def __init__(self, parent=None):
+        """
+        Initialize class
+        """
+        super(landmarkTool, self).__init__(parent=parent)
+        self.setWindowFlags(QtCore.Qt.Window)
+
+        self.widgetPath = os.path.dirname(os.path.abspath(__file__))
+        self.widget = QtUiTools.QUiLoader().load(
+            os.path.join(self.widgetPath, "landmarkTool.ui")
+        )
+        self.widget.setParent(self)
+        # set initial window size
+        self.resize(600, 850)
+        # locate UI widgets
+        self.btn_close = self.widget.findChild(QtWidgets.QPushButton, 'btn_close')
+        # assign functionality to buttons
+        self.btn_close.clicked.connect(self.close)
+
+    def resizeEvent(self, event):
+        """
+        Called on automatically generated resize event
+        """
+        self.widget.resize(self.width(), self.height())
+
+def openWindow():
+    """
+    ID Maya and attach tool window.
+    """
+    # Maya uses this so it should always return True
+    if QtWidgets.QApplication.instance():
+        # Id any current instances of tool and destroy
+        for win in (QtWidgets.QApplication.allWindows()):
+            if 'myToolWindowName' in win.objectName():  # update this name to match name below
+                win.destroy()
+
+    # QtWidgets.QApplication(sys.argv)
+    mayaMainWindowPtr = omui.MQtUtil.mainWindow()
+    mayaMainWindow = wrapInstance(int(mayaMainWindowPtr), QtWidgets.QWidget)
+    landmarkTool.window = landmarkTool(parent=mayaMainWindow)
+    landmarkTool.window.setObjectName('myToolWindowName')  # code above uses this to ID any existing windows
+    landmarkTool.window.setWindowTitle('landmarkTool Ui')
+    landmarkTool.window.show()
